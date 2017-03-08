60b162403d57bc3cfff1d6349d582d40a0989dfd
barbara <barbara@lab804.com.br>
2017-03-08 10:51:23 -0300

    add comentarios e readme

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Web Scraping Gmail
+
+A classe Gmail é responsável por gerenciar automaticamente
+os emails recebidos por um pre determinado remetente.
+Suas funções são: busca por conteudo (neste caso, um link)
+em um email, realizar o download do arquivo a partir do dado
+coletado, deletar o email após o downalod bem sucessido do
+arquivo.
+
+A linguagem utilizada é Python 3
--- gmail_mailbox.py
+++ gmail_mailbox.py
@@ -7,6 +7,7 @@ import getpass
 import requests
 import re, os
 
+
 session = requests.Session()
 
 IMAP_SERVER = 'imap.gmail.com'
@@ -19,38 +20,47 @@ link_re = re.compile(rb'href=\'(.*)?\'')
 class Gmail(object):
 
     def __init__(self, user, password):
+        """Inicialização das variaveis recebidas e conexao com IMAP"""
         self.user = user
         self.password = password
-        self.cont = 0
         if IMAP_USE_SSL:
             self.imap = imaplib.IMAP4_SSL(IMAP_SERVER, IMAP_PORT)
         else:
             self.imap = imaplib.IMAP4(IMAP_SERVER, IMAP_PORT)
 
     def __enter__(self):
+        """Realiza login com usuario e senha, selecionando caixa de entrada geral"""
         self.imap.login(self.user, self.password)
         self.imap.select('Inbox')
         return self
 
     def __exit__(self, type, value, traceback):
+        """Realiza logout da sessao e email"""
         self.imap.close()
         self.imap.logout()
 
     def get_count(self):
+        """Realiza a busca de emails(contabilizando). Pra isso, tem
+        como parametro o email address de um determinado remetente"""
         status, data = self.imap.search(None, FROM)
         return sum(1 for num in data[0].split())
 
     def delete_message(self, num):
+        """É responsável por deletar um email de um determinado remetente"""
         self.imap.store(num, '+FLAGS', r'\Deleted')
         self.imap.expunge()
 
     def delete_all(self):
+        """É responsável por deletar todos emails de um determinado remetente"""
         status, data = self.imap.search(None, FROM)
         for num in data[0].split():
             self.imap.store(num, '+FLAGS', r'\Deleted')
         self.imap.expunge()
 
     def print_msgs(self):
+        """Imprime a quantidade total de emailo e conteudo, além de gerenciar
+        a busca pelo link no email e realizar o download do arquivo.
+        Após isto, deleta o email referente ao arquivo baixado com sucesso"""
         status, data = self.imap.search(None, FROM)
         for num in reversed(data[0].split()):
             status, data = self.imap.fetch(num, '(RFC822)')
@@ -66,26 +76,23 @@ class Gmail(object):
 
     @staticmethod
     def search_link(data):
+        """É responsável pela busca do link de download do arquivo,
+        em meio ao conteudo do email"""
         link = link_re.findall(data)
         result = (b''.join(link).decode())
         print(result)
         return result
 
-    def delete_msgs_sent_to(self, email_address):
-        status, data = self.imap.search(None, 'TO', email_address)
-        if status == 'OK':
-            for num in reversed(data[0].split()):
-                status, data = self.imap.fetch(num, '(RFC822)')
-                self.imap.store(num, '+FLAGS', r'\Deleted')
-        self.imap.expunge()
-
     staticmethod
     def filename(url):
+        """Responsável por nomear o arquivo baixado"""
         link = self.search_name(url)
         print (link)
         return link
 
     def download_file(self, url):
+        """Realiza o download do arquivo atraves do link encaminhado
+        para o email cadastrado."""
         linkname = url.split('/')[-1]
         local_filename = ('%s.csv' % linkname)
         r = session.get(url, stream=True)
